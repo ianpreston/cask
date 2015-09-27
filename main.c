88ea58bd093f9802c519e3ddad7197320bd36725
Ian Preston <hello@ianpreston.io>
2015-09-27 15:48:44 -0700

    Split init_process() into a number of functions
    Replace init_process with init_proc, setup_filesystem, setup_network,
    and run_entry_point. Extract spawn_init_proc() from main().

--- main.c
+++ main.c
@@ -15,13 +15,72 @@
 #define clone_flags CLONE_NEWPID
 #define unshare_flags (CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWUTS)
 
-int init_process(void *root)
+int setup_filesystem(char *root, char *old_root)
 {
-    // Build the path that will contain the filesystem pivot
+    // Mark the root drive as private, so changes in this namespace are not
+    // propagated up to the parent namespace
+    if (mount("", "/", NULL, MS_PRIVATE|MS_REC, NULL) < 0)
+    {
+        printf("setup_filesystem(): Failed to mark root as private\n");
+        return -1;
+    }
+
+    if (mount(root, root, NULL, MS_BIND|MS_REC, NULL) < 0)
+    {
+        printf("setup_filesystem(): Failed to bind /subfs\n");
+        return -1;
+    }
+
+    // Use pivot_root to jail this process inside of `root`
+    if (syscall(SYS_pivot_root, root, old_root)< 0) {
+        printf("setup_filesystem(): pivot_root failed: %d\n", errno);
+        return -1;
+    }
+
+    if (chdir("/") < 0) {
+        printf("setup_filesystem(): chdir failed\n");
+        return -1;
+    }
+
+    // Unmount the pivot point (i.e. make the host's root unreachable from
+    // within the container)
+    if (umount2("/old-root", MNT_DETACH) < 0)
+    {
+        printf("setup_filesystem(): umount2(): %d\n", errno);
+        return -1;
+    }
+
+    return 0;
+}
+
+int setup_network(char *hostname)
+{
+    if (sethostname(hostname, strlen(hostname)) < 0)
+    {
+        printf("setup_network(): Failed to set hostname\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int run_entry_point()
+{
+    char *busybox_argv[] = { "sh", NULL };
+    execvp("/busybox", busybox_argv);
+
+    printf("run_entry_point(): execvp(): %d\n", errno);
+    return -1;
+}
+
+int init_proc(void *root)
+{
+    // Build the path that will contain the filesystem pivot and
+    // ensure it exists on disk
     char *old_root = calloc(strlen(root) + strlen("/old-root") + 1, 1);
     if (old_root == NULL)
     {
-        printf("init_process(): malloc: %d\n", errno);
+        printf("init_proc(): malloc: %d\n", errno);
         return 1;
     }
     strcat(old_root, root);
@@ -31,60 +90,52 @@ int init_process(void *root)
     {
         if (errno != EEXIST)
         {
-            printf("init_process(): mkdir(): %d\n", errno);
+            printf("init_proc(): mkdir(): %d\n", errno);
             return 1;
         }
     }
 
-    // Create new namespaces
+    // Create and enter new namespaces
     if (unshare(unshare_flags) < 0)
     {
-        printf("init_process(): unshare(): %d\n", errno);
+        printf("init_proc(): unshare(): %d\n", errno);
         return 1;
     }
 
-    if (sethostname("sub", 3) < 0)
+    if (setup_network("sub") < 0)
     {
-        printf("init_process(): Failed to set hostname");
+        printf("init_proc(): setup_network() failed\n");
+        return 1;
     }
 
-    // Mark the root drive as private, so changes in this namespace are not
-    // propagated up to the parent namespace
-    if (mount("", "/", NULL, MS_PRIVATE|MS_REC, NULL) < 0)
+    if (setup_filesystem(root, old_root) < 0)
     {
-        printf("init_process(): Failed to mark root as private\n");
+        printf("init_proc(): setup_filesystem() failed\n");
         return 1;
     }
 
-    if (mount(root, root, NULL, MS_BIND|MS_REC, NULL) < 0)
+    return run_entry_point();
+}
+
+int spawn_init_proc(char *root)
+{
+    void *stack = malloc(stack_size);
+    if (stack == 0)
     {
-        printf("init_process(): Failed to bind /subfs\n");
-        return 1;
+        printf("spawn_init_proc(): malloc(): %d\n", errno);
+        return -1;
     }
+    void *stack_base = (void*)((uint64_t)stack + (uint64_t)stack_size); 
 
-    // Use pivot_root to jail this process inside of `root`
-    if (syscall(SYS_pivot_root, root, old_root)< 0) {
-        printf("init_process(): pivot_root failed: %d\n", errno);
-        return 1;
-    }
-
-    if (chdir("/") < 0) {
-        printf("init_process(): chdir failed\n");
-        return 1;
-    }
-
-    // Unmount the pivot point (i.e. make the host's root unreachable from
-    // within the container)
-    if (umount2("/old-root", MNT_DETACH) < 0)
+    pid_t pid = clone(init_proc, stack_base, CLONE_NEWPID | SIGCHLD, root);
+    if (pid < 0)
     {
-        printf("init_process(): umount2(): %d\n", errno);
-        return 1;
+        printf("spawn_init_proc(): clone(): %d\n", errno);
+        return -1;
     }
 
-    char *busybox_argv[] = { "sh", NULL };
-    execvp("/busybox", busybox_argv);
-    printf("init_process(): execvp(): %d\n", errno);
-    return 1;
+    waitpid(pid, NULL, 0);
+    return 0;
 }
 
 int main(int argc, char **argv)
@@ -94,21 +145,12 @@ int main(int argc, char **argv)
         printf("usage: %s <root>\n", argv[0]);
     }
 
-    void *stack = malloc(stack_size);
-    if (stack == 0)
+    char *root = argv[1];
+    if (spawn_init_proc(root) < 0)
     {
-        printf("main(): malloc(): %d\n", errno);
-        return 1;
-    }
-    void *stack_base = (void*)((uint64_t)stack + (uint64_t)stack_size); 
-
-    pid_t pid = clone(init_process, stack_base, CLONE_NEWPID | SIGCHLD, argv[1]);
-    if (pid < 0)
-    {
-        printf("main(): clone(): %d\n", errno);
+        printf("spawn_init_proc() failed\n");
         return 1;
     }
 
-    waitpid(pid, NULL, 0);
     return 0;
 }
