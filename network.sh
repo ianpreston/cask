e2710699837cac9b6da3a5c7cc49e5034960396c
Ian Preston <hello@ianpreston.io>
2015-10-03 13:48:22 -0700

    Add placeholder network.sh script

---
+++ network.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -v
+set -e
+
+if test $# -lt 1; then
+    echo "usage: $0 <pid>"
+    exit
+fi
+
+sudo ip link del veth0
+
+ip link add name veth0 type veth peer name veth1
+ip link set veth1 netns $1
+
+ifconfig veth0 10.1.1.2 up
+python attach.py $1 ifconfig veth1 10.1.1.1 up
